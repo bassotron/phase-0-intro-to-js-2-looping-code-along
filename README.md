07960e6e89def88e34bea0429ec7fb484d228f2e
Will Lovell <willard.lovell@icloud.com>
2021-10-31 16:31:08 -0600

    Completed assignment

--- README.md
+++ README.md
@@ -156,7 +156,7 @@ function wrapGifts(gifts) {
   for (let i = 0; i < gifts.length; i++) {
     console.log(`Wrapped ${gifts[i]} and added a bow!`);
     debugger;
-  }
+  } 
 
   return gifts;
 }
--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+const array = []
+const names = ["Lisa", "Kaitlin", "Jan"];
+const event = ["surprise"];
+
+function writeCards(names, event) {
+     let  array = [] ;
+     for (let i = 0; i < names.length; i++) {
+         array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+         console.log(array)
+        
+        
+         
+        }
+        return array
+        }
+        
+
+function countDown(positiveNumber) {
+    while (positiveNumber >= 0); {
+        positiveNumber--;
+        console.log(positiveNumber);
+    debugger;
+          }
+             return countDown
+         }
+        
+         
+ 
